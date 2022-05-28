532bf5fb44cf5a17ff2a507da2f8fd99bfce4986
Lucas <lucaasferreirafl@gmail.com>
2022-05-27 22:12:03 -0300

    feat: create category

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
-import express, { Request, Response } from "express";
+import express from "express";
+
+/* routes */
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
-app.get("/", (request: Request, response: Response) => {
-  return response.json({
-    hello: "world",
-  });
-});
+app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running on PORT 3333"));

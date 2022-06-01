eb026a28d12abab00f5929312180f18706e69db7
Lucas <lucaasferreirafl@gmail.com>
2022-06-01 01:49:42 -0300

    feat: implements middleware to verify if user auis thenticated

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) throw new Error("Token missing!");
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "9df06d5e77be1b03ba1d7923ad5f681c"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = usersRepository.findById(user_id);
+    if (!user) throw new Error("User does not exists!");
+
+    next();
+  } catch (err) {
+    throw new Error("Invalid token!");
+  }
+}

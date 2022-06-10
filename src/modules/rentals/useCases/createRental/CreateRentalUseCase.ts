cec046cea3c256ca594e5d174494704524c95a68
Lucas <lucaasferreirafl@gmail.com>
2022-06-10 19:22:57 -0300

    feat: create rental

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
@@ -17,7 +18,9 @@ class CreateRentalUseCase {
     @inject("RentalsRepository")
     private rentalsRepository: IRentalsRepository,
     @inject("DayjsDateProvider")
-    private dateProvider: IDateProvider
+    private dateProvider: IDateProvider,
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository
   ) { }
 
   async execute({
@@ -40,18 +43,20 @@ class CreateRentalUseCase {
     if (rentalOpenToUse)
       throw new AppError("This user already have rental open!");
 
-    const rental = await this.rentalsRepository.create({
-      car_id,
-      user_id,
-      expected_return_date,
-    });
-
     const compare = this.dateProvider.diffInHours(
       this.dateProvider.dateNow(),
       expected_return_date
     );
 
     if (compare < minimumHours) throw new AppError("Invalid return time!");
+
+    const rental = await this.rentalsRepository.create({
+      car_id,
+      user_id,
+      expected_return_date,
+    });
+
+    await this.carsRepository.updateAvailable(car_id, false);
 
     return rental;
   }

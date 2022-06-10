c2f359504a68c7cbf48a6a3d6f8708da8948edd2
Lucas <lucaasferreirafl@gmail.com>
2022-06-10 20:04:50 -0300

    feat: devolution rental

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -2,6 +2,7 @@ interface IDateProvider {
   diffInHours(start_date: Date, end_date: Date): number;
   convertToUTC(date: Date): string;
   dateNow(): Date;
+  diffInDays(start_date: Date, end_date: Date): number;
 }
 
 export { IDateProvider };

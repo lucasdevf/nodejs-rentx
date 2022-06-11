aba10a5ec3fe9c3b0e6b30f5bc218a0aaf4c2637
Lucas <lucaasferreirafl@gmail.com>
2022-06-11 16:31:15 -0300

    feat: reset password

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -5,6 +5,7 @@ interface IDateProvider {
   diffInDays(start_date: Date, end_date: Date): number;
   addDays(days: number): Date;
   addHours(hours: number): Date;
+  before(start_date: Date, end_date: Date): boolean;
 }
 
 export { IDateProvider };
